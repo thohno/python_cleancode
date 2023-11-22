dda510bff32c6ee7e2e1a96ed440fdbeddd1570a
Tay, Xinyi <xinyi.tay2@zuhlke.com>
2023-11-22 15:59:12 +0800

    updated reference for sleep mock test exercise

--- src/suggested_solutions/test_sleep_sol.py
+++ src/suggested_solutions/test_sleep_sol.py
@@ -1,6 +1,7 @@
 from unittest.mock import MagicMock
 from src.exercises import sleep
 
+# Reference: https://docs.python.org/3/library/unittest.mock.html
 class TestSleepSol:
     def test_sleep_1h(self):
         sleep.sleep_1h = MagicMock()
